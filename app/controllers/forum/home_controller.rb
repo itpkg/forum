cedbdce09c2827cb5cbae4e45974fd70401654b9
Jitang Zheng <jitang.zheng@gmail.com>
2016-09-06 13:49:31 -0700

    home

--- app/controllers/forum/home_controller.rb
+++ app/controllers/forum/home_controller.rb
@@ -2,6 +2,9 @@ require_dependency "forum/application_controller"
 
 module Forum
   class HomeController < ApplicationController
-
+    def index
+      @articles = Article.order(rate: :desc).limit(32)
+      @article = @articles.first
+    end
   end
 end
